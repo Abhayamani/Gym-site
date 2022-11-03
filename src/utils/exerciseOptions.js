499ab3a6af19740b5248792ad8c61edac5231999
Abhaya Mani Paudel <abhayamanipaudel@gmail.com>
2022-11-03 15:26:03 +0545

    first commit

---
+++ src/utils/exerciseOptions.js
@@ -0,0 +1,10 @@
+const exerciseOptions =  {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+
+        'X-RapidAPI-Key': '108fd9e93bmshad2fa223ff0ea38p18e86bjsn39dec0d3ee77'
+    }
+}
+
+export default exerciseOptions
---
+++ src/utils/youtubeOptions.js
@@ -0,0 +1,9 @@
+const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '108fd9e93bmshad2fa223ff0ea38p18e86bjsn39dec0d3ee77',
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+}
+  
+export default youtubeOptions
